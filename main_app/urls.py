603f3c89aa0598085c9adfa3488b4a9b569284d0
GenioZx <eli.rodri14@email.com>
2025-09-09 11:05:28 -0400

    made migration file, created catmodels.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,6 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('home/', views.home, name ='home'),
   # route for cats index
   path('cats/', views.cat_index, name='cat-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,21 +2,6 @@
 
 from django.shortcuts import render
 
-# Import HttpResponse to send text-based responses
-from django.http import HttpResponse
-
-# Define the home view function
-def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
-
-    # main_app/views.py
-def about(request):
-    return render(request, 'about.html')
-
-
-# views.py
-
 class Cat:
     def __init__(self, name, breed, description, age):
         self.name = name
@@ -32,11 +17,20 @@ cats = [
     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
 ]
 
-# views.py
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return render(request, 'home.html')
+
+    # main_app/views.py
+def about(request):
+    return render(request, 'about.html')
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
     return render(request, 'cats/index.html', {'cats': cats})
 
-def home(request):
-    return render(request, 'home.html')
+
+
+
+
