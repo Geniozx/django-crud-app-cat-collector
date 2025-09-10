7997ae12912c1d1c2938d116e644e7236780f03e
GenioZx <eli.rodri14@email.com>
2025-09-10 10:35:50 -0400

    created cat database and cats detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,8 @@
 # main_app/views.py
 
 from django.shortcuts import render
+from .models import Cat
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 # Define the home view function
 def home(request):
@@ -27,9 +14,14 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
+# views.py
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
 
 
 
